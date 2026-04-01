067a546ea0f71151f6f04b20223f7b4483148776
Guillaume Quelen <quenechg@proton.me>
2026-04-01 18:04:45 +0200

    Ajout score dans models>players

--- models/players.py
+++ models/players.py
@@ -1,14 +1,16 @@
-Class Player:
+class Player:
     def __init__(self, last_name, first_name, birth_date, national_id):
         self.last_name = last_name
         self.first_name = first_name
         self.birth_date = birth_date
         self.national_id = national_id
+        self.score = 0
 
     def to_dict(self):
         return {
             "last_name": self.last_name,
             "first_name": self.first_name,
             "birth_date": self.birth_date,
-            "national_id": self.national_id
+            "national_id": self.national_id,
+            "score": self.score
         }
