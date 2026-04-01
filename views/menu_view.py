8c3f9f03632cc6fbf874020dc1a047a9f60908e1
Guillaume Quelen <quenechg@proton.me>
2026-04-01 17:56:34 +0200

    Résolution conflit menu_view

--- views/menu_view.py
+++ views/menu_view.py
@@ -1,12 +1,8 @@
-<<<<<<< HEAD
 class MenuView:
     def show_menu(self):
-        print("Bienvenue, quel menu souhaiteriez-vous accèder?")
-        print("1.Les joueurs")
-        print("2.Les tournois")
-        print("3.Les rapports")
-        print("4.Quitter")
+        print("Bienvenue, quel menu souhaiteriez-vous accéder?")
+        print("1. Les joueurs")
+        print("2. Les tournois")
+        print("3. Les rapports")
+        print("4. Quitter")
         return input("Votre choix : ")
-=======
-
->>>>>>> 1da38516c7226d00c8904225abc0da6f63f9cf0d
