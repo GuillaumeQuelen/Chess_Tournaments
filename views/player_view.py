a897e33f0364ce59af92086adeba58cae270d7f5
Guillaume Quelen <quenechg@proton.me>
2026-04-07 18:27:48 +0200

    Corrections flake8

--- views/player_view.py
+++ views/player_view.py
@@ -10,16 +10,15 @@ class PlayerView:
         national_id = self.get_national_id()
         return first_name, last_name, birth_date, national_id
 
-
     def ask_continue(self):
         return input("Ajouter un autre joueur ? (O/N) : ").upper() == "O"
-    
+
     def player_menu(self):
         print("\n=== GESTION DES JOUEURS ===")
         print("1. Créer un joueur")
         print("2. Retour au menu principal")
         return input("Votre choix : ")
-    
+
     def get_valid_birth_date(self):
         while True:
             birth_date = input("Date de naissance (JJ/MM/AAAA) : ")
@@ -38,12 +37,14 @@ class PlayerView:
             if re.match(r"^[A-Z]{2}\d{5}$", national_id):
                 return national_id
             print("Format invalide ! Ex: AB12345")
+
     def get_valid_first_name(self):
         while True:
             first_name = input("Entrez le prénom du joueur : ")
             if first_name.strip():
                 return first_name
             print("Le prénom est obligatoire !")
+
     def get_valid_last_name(self):
         while True:
             last_name = input("Entrez le nom du joueur : ")
